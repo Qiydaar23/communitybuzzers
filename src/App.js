a014cfbba9d97a6dc42eb2a9e3ff81ca4e469edd
Qiydaarwilliams23@gmail.com <Qiydaarwilliams23@gmail.com>
2024-08-27 13:06:22 -0400

    api fully connected GET/PUT/DELET

--- src/App.js
+++ src/App.js
@@ -7,6 +7,9 @@ function App() {
 
   const [posts, setPosts] = useState([])
 
+  const [comment , setComment] = useState("")
+  const [titleName , setTitleName] = useState("")
+
   const url = "http://127.0.0.1:8000/api/posts/"
 
   const fetchPosts = async () =>{
@@ -23,13 +26,30 @@ function App() {
     fetchPosts()
   },[])
 
+  const addComment = async () => {
+
+    const commentData = {
+
+      title: titleName,
+      content: comment
+    }
+    const response = await fetch("http://127.0.0.1:8000/api/posts/create",{
+      method: "POST",
+      headers: {'Content-Type': 'application/json'},
+      body: JSON.stringify(commentData)
+    })
+    const data = await response.json()
+    setPosts((prev) => [...prev, data])
+  }
+
 
   return (
     <div className="App">
       <h1>Posts</h1>
       <div>
-        <input type="text" placeholder=' post...'></input>
-        <button>POST</button>
+        <input type="text" placeholder=' title...' onChange={(e)=>setTitleName(e.target.value)}></input>
+        <input type="text" placeholder=' comment...' onChange={(e)=>setComment(e.target.value)}></input>
+        <button onClick={addComment}>POST</button>
       </div>
       {posts.map((post) => {
         const {id, title, content } = post
@@ -38,6 +58,8 @@ function App() {
           <div key={id}>
             <h1>{title}</h1>
             <h4>{content}</h4>
+            <input type='text' placeholder='edit content...'/>
+            <button>edit comment</button>
           </div>
         )
       })}
